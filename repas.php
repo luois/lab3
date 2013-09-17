653adae2784e1783ef90346ee1770f2f0677c085
luois <lpink_11@hotmail.com>
2013-09-17 11:14:10 -0400

    base de données

--- repas.php
+++ repas.php
@@ -1,9 +1,4 @@
-
-<html>
-<head>
-    <title>Repas</title>
-</head>
-<body><?php
+<?php
 if(!($connect = mysql_connect ('localhost', 'root', 'admin123'))) {
     echo 'La connexion a été établi';
 
@@ -11,13 +6,23 @@ if(!($connect = mysql_connect ('localhost', 'root', 'admin123'))) {
 
 mysql_select_db ('repas', $connect) ;
 ?>
+<meta http-equiv="content-type" content="text/html" charset="UTF-8">
+<meta http-equiv="content-type" content="text/html" charset="ISO-8859-1">
+<html>
+
+<head>
+    <title>Repas</title>
+</head>
+<body>
+<h1>SA marche!</h1>
 <?php
-$sql ="SELECT * FROM Pays";
-mysql_query($sql);
+$sql ="SELECT pays FROM Pays ";
+$request = mysql_query($sql);
 echo '<select>';
-while($result = mysql = mysql_fetch_assoc($sql)){
+$result = mysql = mysql_fetch_assoc($request);
+
     echo '<option value="'.$result['pays'].'">'.$result[''].'</option>';
-}
+
 mysql_close($connect);
 ?>
 </body>
