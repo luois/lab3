4956e8975697774ff7dc8fd22545ee3a55e4e8c2
luois <lpink_11@hotmail.com>
2013-09-17 11:31:38 -0400

    base de données

--- repas.php
+++ repas.php
@@ -1,15 +1,10 @@
 <?php
-if(!($connect = mysql_connect ('localhost', 'root', 'admin123'))) {
-    echo 'La connexion a été établi';
-
-}// or die("Impossible de se connecter à la base de données")or die("Impossible de se connecter à la base de données");
+$connect = mysql_connect ('localhost', 'root', 'admin123') or die("Impossible de se connecter à la base de données")or die("Impossible de se connecter à la base de données");
 
 mysql_select_db ('repas', $connect) ;
 ?>
-<meta http-equiv="content-type" content="text/html" charset="UTF-8">
 <meta http-equiv="content-type" content="text/html" charset="ISO-8859-1">
 <html>
-
 <head>
     <title>Repas</title>
 </head>
@@ -19,10 +14,10 @@ mysql_select_db ('repas', $connect) ;
 $sql ="SELECT pays FROM Pays ";
 $request = mysql_query($sql);
 echo '<select>';
-$result = mysql = mysql_fetch_assoc($request);
-
-    echo '<option value="'.$result['pays'].'">'.$result[''].'</option>';
-
+while($result = mysql_fetch_assoc($request))
+{
+    echo '<option value="'.$result['pays'].'">'.$result['pays'].'</option>';
+}
 mysql_close($connect);
 ?>
 </body>
