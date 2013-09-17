12ef47b12bad0790170888dadd50484b999f9bb5
luois <lpink_11@hotmail.com>
2013-09-17 11:06:59 -0400

    base de données

--- repas.php
+++ repas.php
@@ -1,16 +1,24 @@
-<?php
-$base = mysql_connect ('localhost', 'root', 'admin123') or die("Impossible de se connecter à la base de données")or die("Impossible de se connecter à la base de données");
 
-mysql_select_db ('repas', $base) ;
-?>
 <html>
 <head>
     <title>Repas</title>
 </head>
-<body>
-<?php
-$sql ="SELECT pays FROM Pays() WHERE "
+<body><?php
+if(!($connect = mysql_connect ('localhost', 'root', 'admin123'))) {
+    echo 'La connexion a été établi';
 
+}// or die("Impossible de se connecter à la base de données")or die("Impossible de se connecter à la base de données");
+
+mysql_select_db ('repas', $connect) ;
+?>
+<?php
+$sql ="SELECT * FROM Pays";
+mysql_query($sql);
+echo '<select>';
+while($result = mysql = mysql_fetch_assoc($sql)){
+    echo '<option value="'.$result['pays'].'">'.$result[''].'</option>';
+}
+mysql_close($connect);
 ?>
 </body>
 </html>
