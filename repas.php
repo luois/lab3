46dfc36303efd1c39c28cb74fda7f80f1a7b076c
luois <lpink_11@hotmail.com>
2013-09-20 13:12:18 -0400

    base de données

--- repas.php
+++ repas.php
@@ -3,9 +3,12 @@ $connect = mysql_connect ('localhost', 'root', 'admin123') or die("Impossible de
 
 mysql_select_db ('repas', $connect) ;
 ?>
-<meta http-equiv="content-type" content="text/html" charset="ISO-8859-1">
+
 <html>
+
 <head>
+    <meta http-equiv="content-type" content="text/html" charset="ISO-8859-1">
+    <link href="../css/bootstrap.css" rel="stylesheet" media="screen">
     <title>Repas</title>
 </head>
 <body>
@@ -29,6 +32,18 @@ while($result = mysql_fetch_array($request))
 {
     echo '<option value="'.$result['Type'].'">'.$result['type'].'</option>';
 }
+//Type select repas
+$sql ="SELECT Repas FROM Type ";
+$request = mysql_query($sql);
+echo '<br><select>';
+while($result = mysql_fetch_array($request))
+{
+    echo '<option value="'.$result['Type'].'">'.$result['type'].'</option>';
+}
+// Nom du repas
+
+//Prix du repas
+
 mysql_close($connect);
 echo '</select>';
 ?>
